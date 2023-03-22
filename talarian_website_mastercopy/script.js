40f5651cf77dfac764e7fd288c3b03608a7282dc
Sheridan Lugo <86641939+Netherite@users.noreply.github.com>
2023-03-21 23:08:50 -0700

    testing

---
+++ talarian_website_mastercopy/script.js
@@ -0,0 +1,20 @@
+//query selector
+//document.querySelector("#products").style.color = "red"; 
+
+//querySelectorALL by nav a 
+//document.querySelectorAll(".navbar a")[1].style.color = "blue"; 
+
+
+//get element by ID 
+//document.getElementById("products").style.color = "red"; 
+
+//get element by parent ID 
+//document.getElementById("navbarside").style.color = "red"; 
+
+//get element by tag 
+//document.getElementsByTagName("span")[0].style.color = "red"; 
+
+//get element by class 
+//document.getElementsByClassName("navbar")[0].getElementsByClassName.color = "red"; 
+
+document.querySelector(".navbar").classList.add("red"); 
