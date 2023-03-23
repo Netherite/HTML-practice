e8f65adb64731ae93cea9b1ce8e33a73730dacba
Sheridan Lugo <86641939+Netherite@users.noreply.github.com>
2023-03-23 16:53:37 -0700

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,44 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+
+
+// === Image 1 randomization ===
+if(randomNumber1 === 1){
+    document.querySelector(".img1").setAttribute("src", "dice1.png");
+}
+else if(randomNumber1 === 2){
+    document.querySelector(".img1").setAttribute("src", "dice2.png");
+}
+else if(randomNumber1 === 3){
+    document.querySelector(".img1").setAttribute("src", "dice3.png");
+}
+else if(randomNumber1 === 4){
+    document.querySelector(".img1").setAttribute("src", "dice4.png");
+}
+else if(randomNumber1 === 5){
+    document.querySelector(".img1").setAttribute("src", "dice5.png");
+}
+else{
+    document.querySelector(".img1").setAttribute("src", "dice6.png");
+}
+
+randomNumber1 = Math.floor(Math.random() * 6 + 1);
+
+// === Image 1 randomization ===
+if(randomNumber1 === 1){
+    document.querySelector(".img2").setAttribute("src", "dice1.png");
+}
+else if(randomNumber1 === 2){
+    document.querySelector(".img2").setAttribute("src", "dice2.png");
+}
+else if(randomNumber1 === 3){
+    document.querySelector(".img2").setAttribute("src", "dice3.png");
+}
+else if(randomNumber1 === 4){
+    document.querySelector(".img2").setAttribute("src", "dice4.png");
+}
+else if(randomNumber1 === 5){
+    document.querySelector(".img2").setAttribute("src", "dice5.png");
+}
+else{
+    document.querySelector(".img2").setAttribute("src", "dice6.png");
+}
