dbc08714909401d1b932364898d814169d808f34
Sheridan Lugo <sheridanlugo@gmail.com>
2023-04-21 13:41:17 -0700

    Getting sphere to show up

--- ugh/ugh1/main.js
+++ ugh/ugh1/main.js
@@ -1,17 +1,34 @@
 import * as THREE from 'three';
 
+//scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
 
-camera.position.z = 5;
+//sphere
+const geometry = new THREE.SphereGeometry( 5, 64, 64 );
+const material = new THREE.MeshBasicMaterial( { color: 0xFF5733 } );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+ /*move camera back by 5 so that camera is not
+ on top of sphere. The size of the objects that appear on the screen 
+ will be relative to camera distance. The smaller the camera, the larger the objects will appear. */ 
 
-function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-}
-animate();
+// Camera, so that things will show up on the screen 
+const camera = new THREE.PerspectiveCamera( 45, 800 / 600, 0.1, 1000 );
+camera.position.z = 20;
+scene.add(camera);
+
+//renderer
+const canvas = document.querySelector('.webgl'); 
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize( 800, 600 ); //renders size of camera
+renderer.render(scene, camera); 
+
+ //light
+ const light = new THREE.PointLight( 0xff0000, 1, 100 );
+ light.position.set( 50, 50, 50 );
+ light.castShadow = true; 
+ scene.add( light );
+
+
+
