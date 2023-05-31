e7948da5515c5ea98f189bc64a78b86cef8bf3e6
Sheridan Lugo <sheridanlugo@gmail.com>
2023-05-31 16:50:01 -0700

    Completion of calculator project, additions to drum project

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,15 +1,16 @@
+
+
 //figuring out how many buttons have the class .drum and saving that number to numberOfDrumButtons 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length; 
 
 //Creating a loop that will iterate through all instances of .drum button
 for(var i = 0; i < numberOfDrumButtons; i++){
-    //selecting all elements with .drum class, using i in place of that array, then adding event listener with click event and anonymous function that creates an alert
-    document.querySelectorAll('.drum')[i].addEventListener("click", function handleClick(){
-        alert("I got clicked"); 
-    }); 
+    //selecting all elements with .drum class, using i in place of that array, then adding event listener with click event and play method for audio file
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+    });
 }
-
-//adding event listener to button
 
 
  
