d30886aae9e691efd546494633d34ebcc699b600
Sheridan Lugo <sheridanlugo@gmail.com>
2023-06-07 15:01:36 -0700

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,4 @@
 
-
 //figuring out how many buttons have the class .drum and saving that number to numberOfDrumButtons 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length; 
 
@@ -7,10 +6,47 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i = 0; i < numberOfDrumButtons; i++){
     //selecting all elements with .drum class, using i in place of that array, then adding event listener with click event and play method for audio file
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+        
+        var buttonInnerHtml = this.innerHTML; 
+
+        switch(buttonInnerHtml){
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play(); 
+                break;
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play(); 
+                break;
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play(); 
+                break;
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play(); 
+                break;
+            case "j":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play(); 
+                break;
+            case "k":
+                var kickBass = new Audio("sounds/kick-bass.mp3");
+                kickBass.play(); 
+                break;
+            case "l":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play(); 
+                break;
+            
+            default: console.log(buttonInnerHtml); 
+        }
     });
+     
 }
 
 
- 
+/* var audio = new Audio("sounds/tom-1.mp3");
+audio.play();
+}); */
+
