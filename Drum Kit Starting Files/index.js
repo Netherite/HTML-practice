75fd8e660cb01a4f347b5d3ca950492dc37ae69c
Sheridan Lugo <sheridanlugo@gmail.com>
2023-06-09 10:52:02 -0700

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,15 +2,15 @@
 //figuring out how many buttons have the class .drum and saving that number to numberOfDrumButtons 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length; 
 
-//Creating a loop that will iterate through all instances of .drum button
-for(var i = 0; i < numberOfDrumButtons; i++){
-    //selecting all elements with .drum class, using i in place of that array, then adding event listener with click event and play method for audio file
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        
-        var buttonInnerHtml = this.innerHTML; 
 
-        switch(buttonInnerHtml){
-            case "w":
+//watching for keyboard event which is pretty automatic
+document.addEventListener("keypress", function(event){
+    makeSound(event.key); 
+})
+
+function makeSound(key){
+    switch(key){
+        case "w":
                 var tom1 = new Audio("sounds/tom-1.mp3");
                 tom1.play(); 
                 break;
@@ -40,13 +40,23 @@ for(var i = 0; i < numberOfDrumButtons; i++){
                 break;
             
             default: console.log(buttonInnerHtml); 
-        }
-    });
-     
+    }
 }
 
 
 /* var audio = new Audio("sounds/tom-1.mp3");
 audio.play();
 }); */
+
+//Creating a loop that will iterate through all instances of .drum button
+for(var i = 0; i < numberOfDrumButtons; i++){
+    //selecting all elements with .drum class, using i in place of that array, then adding event listener with click event and play method for audio file
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        
+        var buttonInnerHtml = this.innerHTML; 
+
+        makeSound(buttonInnerHTML); 
+    });
+     
+}
 
